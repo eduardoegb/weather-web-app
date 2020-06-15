de5005fe718713162cb812af7e7c13c34b8023fb
Eduardo Gonzalez <eduardoegb@gmail.com>
2020-06-15 17:35:52 -0400

    Adding credentials

---
+++ src/credentials.js
@@ -0,0 +1,9 @@
+const credentials = {
+  TOKEN: 'pk.eyJ1IjoiZWR1YXJkb2VnYiIsImEiOiJja2JjZHZic3cwMHh1MnJtdW8wNmd6NmFoIn0.q53N30EM4x38fwkG-jHesQ',
+  ACCESS_KEY: '1f04d4673a5b572ad45997ebb2b08edd'
+};
+
+module.exports = {
+  ACCESS_KEY: credentials.ACCESS_KEY,
+  TOKEN: credentials.TOKEN
+};
